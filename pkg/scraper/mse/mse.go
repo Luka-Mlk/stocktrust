013a2d3c590d770c364ad2b03f0d9006dc9cc418
Luka-Mlk <luka.milik1@gmail.com>
2024-11-08 18:57:38 +0100

    removed excess comments

--- pkg/scraper/mse/mse.go
+++ pkg/scraper/mse/mse.go
@@ -7,22 +7,15 @@ import (
 )
 
 func Init() error {
-	// dbtkrs := map[string]date
-	// if dbtkrs != empty
-	// goroutine for ticker history that already exists in the dbtkrs map?
-
-	// else map empty execute code below
 	tkrs, err := GetTickers()
 	if err != nil {
 		log.Println(err)
 		debug.PrintStack()
 		return err
 	}
-	// update function call here? on history
 	for _, tkr := range tkrs {
 		latestDate, err := hrecord.GetLatestTkrDate(tkr)
 		if err != nil && err.Error() == "record for ticker not found" {
-			// Check if invalid company is exact error
 			err = getCompanyFromTicker(tkr)
 			if err != nil {
 				continue
