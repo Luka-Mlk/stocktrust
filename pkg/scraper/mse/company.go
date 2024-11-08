7c4e98a22b090fdad0857278dda41c0118b66ffa
Luka-Mlk <luka.milik1@gmail.com>
2024-11-08 20:13:20 +0100

    transfter changes to new branch

---
+++ pkg/scraper/mse/company.go
@@ -0,0 +1,124 @@
+package scraper
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"runtime/debug"
+	"stocktrust/pkg/company"
+	"stocktrust/pkg/queue"
+	compfmt "stocktrust/pkg/strings/formatter/company"
+
+	"github.com/gocolly/colly"
+)
+
+const (
+	accordionSeletor   string = "#collapseFive a:nth-child(1)"
+	companyDataWrapper string = "#izdavach"
+)
+
+func getCompanyFromTicker(tkr string) error {
+	lookupURL := fmt.Sprintf("https://www.mse.mk/mk/search/%s", tkr)
+	c := colly.NewCollector()
+	c.Limit(&colly.LimitRule{
+		DomainGlob: "*mse.mk*",
+	})
+	cc := c.Clone()
+	var cerr error
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println(err)
+		cerr = err
+	})
+	if cerr != nil {
+		log.Println(cerr)
+		return cerr
+	}
+	c.OnHTML(accordionSeletor, func(h *colly.HTMLElement) {
+		companyUrl := h.Attr("href")
+		cc.Visit("https://mse.mk" + companyUrl)
+	})
+	cc.OnError(func(r *colly.Response, err error) {
+		log.Println(err)
+		cerr = err
+	})
+	if cerr != nil {
+		log.Println(cerr)
+		return cerr
+	}
+	var (
+		name         string
+		address      string
+		city         string
+		country      string
+		email        string
+		website      string
+		contactName  string
+		contactPhone string
+		contactEmail string
+		phone        string
+		fax          string
+		prospect     string
+		url          string
+	)
+	cc.OnHTML(companyDataWrapper, func(h *colly.HTMLElement) {
+		name = h.ChildText(".title")
+		if name == "" {
+			return
+		}
+		address = h.ChildText(companyDataWrapper + " > .row:nth-child(3) .col-md-8")
+		if address == "" {
+			return
+		}
+		city = h.ChildText(".row:nth-child(4) .col-md-8")
+		country = h.ChildText(".row:nth-child(5) .col-md-8")
+		email = h.ChildText(".row:nth-child(6) .col-md-8")
+		website = h.ChildText(".row:nth-child(7) .col-md-8")
+		contactName = h.ChildText("#popover-content .row:nth-child(1) .col-md-8")
+		contactPhone = h.ChildText("#popover-content .row:nth-child(2) .col-md-8")
+		contactEmail = h.ChildText("#popover-content .row:nth-child(3) .col-md-8")
+		phone = h.ChildText(".row:nth-child(9) .col-md-8")
+		fax = h.ChildText(".row:nth-child(10) .col-md-8")
+		prospect = h.ChildAttr(".row:nth-child(11) .col-md-8 a", "href")
+		url = h.Request.URL.String()
+	})
+	if cerr != nil {
+		return cerr
+	}
+	c.Visit(lookupURL)
+	cmp, err := company.NewCompany(
+		company.WithTicker(tkr),
+		company.WithPersistence(&company.SQLPersistence{}),
+	)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	cmp.Name = name
+	cmp.Address = address
+	cmp.City = city
+	cmp.Country = country
+	cmp.Email = email
+	cmp.Website = website
+	cmp.ContactName = contactName
+	cmp.ContactPhone = contactPhone
+	cmp.ContactEmail = contactEmail
+	cmp.Phone = phone
+	cmp.Fax = fax
+	cmp.Prospect = prospect
+	cmp.URL = url
+	if name == "" || address == "" {
+		return errors.New("invalid company")
+	}
+	compfmt.Company(cmp)
+
+	var q queue.Queue
+	q.Enqueue(cmp)
+
+	err = cmp.Save()
+	if err != nil {
+		log.Println(err)
+		debug.PrintStack()
+		return err
+	}
+	return nil
+}
---
+++ pkg/scraper/mse/mse.go
@@ -0,0 +1,44 @@
+package scraper
+
+import (
+	"log"
+	"runtime/debug"
+	"stocktrust/pkg/hrecord"
+)
+
+func Init() error {
+	tkrs, err := GetTickers()
+	if err != nil {
+		log.Println(err)
+		debug.PrintStack()
+		return err
+	}
+
+	for _, tkr := range tkrs {
+		latestDate, err := hrecord.GetLatestTkrDate(tkr)
+		if err != nil && err.Error() == "record for ticker not found" {
+			err = getCompanyFromTicker(tkr)
+			if err != nil {
+				continue
+			}
+			err = getHrListForTicker(tkr)
+			if err != nil {
+				log.Println(err)
+				debug.PrintStack()
+				return err
+			}
+		} else if err != nil {
+			log.Println(err)
+			debug.PrintStack()
+			return err
+		} else {
+			err = updateHrForTicker(tkr, latestDate)
+			if err != nil {
+				log.Println(err)
+				debug.PrintStack()
+				return err
+			}
+		}
+	}
+	return nil
+}
