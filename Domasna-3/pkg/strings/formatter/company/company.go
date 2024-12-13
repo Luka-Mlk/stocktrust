22e504486a1fafd4877c20ef1b1b40b9c42ee566
Luka-Mlk <luka.milik1@gmail.com>
2024-12-13 03:12:24 +0100

    add homework 3 for sync to remote

---
+++ Domasna-3/pkg/strings/formatter/company/company.go
@@ -0,0 +1,38 @@
+package compfmt
+
+import (
+	"stocktrust/pkg/company"
+	"strings"
+)
+
+func Company(cmp *company.Company) {
+	if cmp.Address == "" {
+		cmp.Address = "/"
+		cmp.City = "/"
+		cmp.Country = "/"
+		cmp.Email = "/"
+		cmp.Website = "/"
+		cmp.ContactName = "/"
+		cmp.ContactPhone = "/"
+		cmp.ContactEmail = "/"
+		cmp.Phone = "/"
+		cmp.Fax = "/"
+		cmp.Prospect = "/"
+		return
+	}
+	scn := strings.Split(cmp.ContactName, "\n")
+	for i, v := range scn {
+		scn[i] = strings.TrimSpace(v)
+	}
+	cmp.ContactName = strings.Join(scn, ",")
+	scp := strings.Split(cmp.ContactPhone, "\n")
+	for i, v := range scp {
+		scp[i] = strings.TrimSpace(v)
+	}
+	cmp.ContactPhone = strings.Join(scp, ",")
+	sce := strings.Split(cmp.ContactEmail, "\n")
+	for i, v := range sce {
+		sce[i] = strings.TrimSpace(v)
+	}
+	cmp.ContactEmail = strings.Join(sce, ",")
+}
